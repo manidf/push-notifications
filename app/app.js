47c424a0d9efcfc0883f8a9517b59d6c0e80d154
manidf <mannuel.ferreira@gmail.com>
2016-01-12 16:51:33 +0200

    added service worker

---
+++ app/app.js
@@ -0,0 +1,12 @@
+// Register and install a Sevice Worker
+
+if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+
+    navigator.serviceWorker.register('/sw.js')
+        .then(function(reg) {
+            console.log(':^)', reg);
+        }).catch(function(err) {
+            console.log(':^(', err);
+    });
+}
---
+++ app/sw.js
@@ -0,0 +1,3 @@
+// When a push message is received, the browser can run a Service Worker in the
+// background to handle push messages without requiring a web page to be open.
+
