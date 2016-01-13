0b9ac48429c99c13e93b448f3b3035d48cdff6c6
manidf <mannuel.ferreira@gmail.com>
2016-01-13 09:06:14 +0200

    added eslint, gulp

--- app/sw.js
+++ app/sw.js
@@ -1,3 +1,20 @@
 // When a push message is received, the browser can run a Service Worker in the
 // background to handle push messages without requiring a web page to be open.
 
+console.log('Started', self);
+
+// self refers to the ServiceWorkerGlobalScope object: the Service worker itself
+self.addEventListener('install', function(event) {
+    // Service worker will skip the waiting state and immediately activate
+    self.skipWaiting();
+    console.log('Installed', event);
+});
+
+self.addEventListener('activate', function(event) {
+   console.log('Activated', event);
+});
+
+self.addEventListener('push', function(event) {
+   console.log('Push message recieved', event);
+    // TODO
+});
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+
+gulp.task('eslint', function() {
+	return gulp.src(['app/js/**/*js'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['eslint'], function() {
+	// runs eslint
+});
